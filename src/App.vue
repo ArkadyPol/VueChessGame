fbf9a3b513d3425f7968e8c7fd6a5f25150d41c3
Arkady Polhovsky <arkims@inbox.ru>
2020-06-10 04:47:54 +0300

    install dependencies

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+
+module.exports = {
+  context: path.resolve(__dirname, "src"),
+  entry: "./main.ts",
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        include: path.resolve(__dirname, "src"),
+        use: {
+          loader: "vue-loader",
+          options: {
+            loaders: {
+              ts: "ts-loader",
+            },
+            esModule: true,
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        include: path.resolve(__dirname, "src"),
+        use: {
+          loader: "ts-loader",
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, "src"),
+        use: ["vue-style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./index.html",
+    }),
+    new VueLoaderPlugin(),
+  ],
+  resolve: {
+    extensions: [".js", ".ts"],
+    plugins: [PnpWebpackPlugin],
+  },
+  resolveLoader: {
+    plugins: [PnpWebpackPlugin.moduleLoader(module)],
+  },
+};
