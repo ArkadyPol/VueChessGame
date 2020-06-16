30cce80af02ac90deb2cb9610aa99ceb539d42b6
Arkady Polhovsky <arkims@inbox.ru>
2020-06-17 02:19:53 +0300

    refactor arange func

--- src/chess/index.ts
+++ src/chess/index.ts
@@ -8,23 +8,20 @@ import Queen from "./chess-piece/Queen";
 import King from "./chess-piece/King";
 export const hor = ["a", "b", "c", "d", "e", "f", "g", "h"] as Horizontal[];
 
+const arrangeValuablePieces = (y: 1 | 8, color: "white" | "black") => {
+  new Rook({ x: "a", y }, color);
+  new Knight({ x: "b", y }, color);
+  new Bishop({ x: "c", y }, color);
+  new Queen({ x: "d", y }, color);
+  new King({ x: "e", y }, color);
+  new Bishop({ x: "f", y }, color);
+  new Knight({ x: "g", y }, color);
+  new Rook({ x: "h", y }, color);
+};
+
 const arrangeChessPieces = () => {
-  new Rook({ x: "a", y: 8 }, "black");
-  new Knight({ x: "b", y: 8 }, "black");
-  new Bishop({ x: "c", y: 8 }, "black");
-  new Queen({ x: "d", y: 8 }, "black");
-  new King({ x: "e", y: 8 }, "black");
-  new Bishop({ x: "f", y: 8 }, "black");
-  new Knight({ x: "g", y: 8 }, "black");
-  new Rook({ x: "h", y: 8 }, "black");
-  new Rook({ x: "a", y: 1 }, "white");
-  new Knight({ x: "b", y: 1 }, "white");
-  new Bishop({ x: "c", y: 1 }, "white");
-  new Queen({ x: "d", y: 1 }, "white");
-  new King({ x: "e", y: 1 }, "white");
-  new Bishop({ x: "f", y: 1 }, "white");
-  new Knight({ x: "g", y: 1 }, "white");
-  new Rook({ x: "h", y: 1 }, "white");
+  arrangeValuablePieces(8, "black");
+  arrangeValuablePieces(1, "white");
   for (let i = 0; i < 8; i++) {
     new BlackPawn({ x: hor[i], y: 7 });
     new WhitePawn({ x: hor[i], y: 2 });
