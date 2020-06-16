b27f47dac7e47c5a2a31264b435e5b4983ba34e3
Arkady Polhovsky <arkims@inbox.ru>
2020-06-17 00:48:18 +0300

    add class Rook

---
+++ src/chess/chess-piece/Rook.ts
@@ -0,0 +1,48 @@
+import ChessPiece from ".";
+import { Position, ChessCode, Horizontal, Vertical } from "@/types";
+import { hor } from "..";
+
+export default class Rook extends ChessPiece {
+  constructor(position: Position, color: "white" | "black") {
+    super(
+      position,
+      color === "white" ? ChessCode.WhiteRook : ChessCode.BlackRook
+    );
+  }
+
+  protected move(chessX: Horizontal, chessY: Vertical) {
+    let id = this.allowedMoves.findIndex(
+      (pos) => pos.x === chessX && pos.y === chessY
+    );
+    if (id === -1) return false;
+    let otherPiece = ChessPiece.findPiece(chessX, chessY);
+    if (otherPiece) {
+      ChessPiece.deletePiece(chessX, chessY);
+    }
+  }
+
+  get allowedMoves() {
+    let moves = [] as Position[];
+    let numX = hor.indexOf(this.x);
+    for (let i = 1; this.y - i > 0; i++) {
+      moves.push({ x: hor[numX], y: (this.y - i) as Vertical });
+      if (ChessPiece.findPiece(hor[numX], (this.y - i) as Vertical)) break;
+    }
+    for (let i = 1; this.y + i < 9; i++) {
+      moves.push({ x: hor[numX], y: (this.y + i) as Vertical });
+      if (ChessPiece.findPiece(hor[numX], (this.y + i) as Vertical)) break;
+    }
+    for (let i = 1; numX - i > -1; i++) {
+      moves.push({ x: hor[numX - i], y: this.y });
+      if (ChessPiece.findPiece(hor[numX - i], this.y)) break;
+    }
+    for (let i = 1; numX + i < 8; i++) {
+      moves.push({ x: hor[numX + i], y: this.y });
+      if (ChessPiece.findPiece(hor[numX + i], this.y)) break;
+    }
+
+    return moves.filter(
+      (pos) => ChessPiece.findPiece(pos.x, pos.y)?.color !== this.color
+    );
+  }
+}
--- src/chess/index.ts
+++ src/chess/index.ts
@@ -4,25 +4,26 @@ import { ChessCode, Horizontal } from "@/types";
 import BlackPawn from "./chess-piece/BlackPawn";
 import Knight from "./chess-piece/Knight";
 import Bishop from "./chess-piece/Bishop";
+import Rook from "./chess-piece/Rook";
 export const hor = ["a", "b", "c", "d", "e", "f", "g", "h"] as Horizontal[];
 
 const arrangeChessPieces = () => {
-  new ChessPiece({ x: "a", y: 8 }, ChessCode.BlackRook);
+  new Rook({ x: "a", y: 8 }, "black");
   new Knight({ x: "b", y: 8 }, "black");
   new Bishop({ x: "c", y: 8 }, "black");
   new ChessPiece({ x: "d", y: 8 }, ChessCode.BlackQueen);
   new ChessPiece({ x: "e", y: 8 }, ChessCode.BlackKing);
   new Bishop({ x: "f", y: 8 }, "black");
   new Knight({ x: "g", y: 8 }, "black");
-  new ChessPiece({ x: "h", y: 8 }, ChessCode.BlackRook);
-  new ChessPiece({ x: "a", y: 1 }, ChessCode.WhiteRook);
+  new Rook({ x: "h", y: 8 }, "black");
+  new Rook({ x: "a", y: 1 }, "white");
   new Knight({ x: "b", y: 1 }, "white");
   new Bishop({ x: "c", y: 1 }, "white");
   new ChessPiece({ x: "d", y: 1 }, ChessCode.WhiteQueen);
   new ChessPiece({ x: "e", y: 1 }, ChessCode.WhiteKing);
   new Bishop({ x: "f", y: 1 }, "white");
   new Knight({ x: "g", y: 1 }, "white");
-  new ChessPiece({ x: "h", y: 1 }, ChessCode.WhiteRook);
+  new Rook({ x: "h", y: 1 }, "white");
   for (let i = 0; i < 8; i++) {
     new BlackPawn({ x: hor[i], y: 7 });
     new WhitePawn({ x: hor[i], y: 2 });
